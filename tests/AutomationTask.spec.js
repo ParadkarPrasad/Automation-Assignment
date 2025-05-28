0931ab5687ba149ecc36dc1d819b1d7bbdb191bc
ParadkarPrasad <prasadparad37@gmail.com>
2025-05-28 23:14:52 +0100

    Assignment Task

---
+++ tests/AutomationTask.spec.js
@@ -0,0 +1,72 @@
+import { test, expect, describe } from '@playwright/test';
+
+describe('New User Registration', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/login')
+  })
+
+  // Sign Up
+  test("New User Sign Up", async ({ page }) => {
+    await page.click('.subLink');
+    await expect(page).toHaveURL('/signup')
+
+    // Assertions
+    const nameInput = page.locator('#name')
+    // Visibility check for the name field
+    await expect(nameInput).toBeVisible()
+    // Enabled state check: Make sure the field is enabled and can be typed into
+    await expect(nameInput).toBeEnabled()
+    // Type into the name field
+    await nameInput.fill("Jes")
+    expect(await nameInput.inputValue()).toBe('Jes')
+
+    const emailInput = page.locator('#email')
+    // Visibility check for the email field
+    await expect(emailInput).toBeVisible()
+    // Enabled state check: Make sure the field is enabled and can be typed into
+    await expect(emailInput).toBeEnabled()
+    // Type into the email field
+    await emailInput.fill("jes123@gmail.com")
+    expect(await emailInput.inputValue()).toBe('jes123@gmail.com')
+
+    const passwordInput = page.locator('#password')
+    // Visibility check for the password field
+    await expect(passwordInput).toBeVisible()
+    // Enable state check: Make sure the field is enabled and can be typed into
+    await expect(passwordInput).toBeEnabled()
+    // Type into the password Field
+    await passwordInput.fill("Automation")
+    expect(await passwordInput.inputValue()).toBe('Automation')
+
+    // CheckBox
+
+    const checkboxLocators = [
+      '[for="683616c6936adb25136a9597"]',
+      '[for="67fc818c148027b98a9af15d"]',
+      '[for="67fc8181148027b98a9af153"]'
+    ]
+
+    for (const locator of checkboxLocators) {
+      await page.locator(locator).click()
+    }
+
+    // Radio Buttons
+    const maleOption = page.locator('#gender1')
+    // Verify the male radio button visible on the page
+    await expect(maleOption).toBeVisible()
+    await maleOption.click();
+    await expect(maleOption).toBeChecked()
+    // Dropdown selection
+    await page.locator('#state').selectOption({ label: 'Maharashtra' })
+
+
+    // Multidropdown selection
+    await page.selectOption('#hobbies', ['Reading', 'Swimming'])
+
+    // Sign up button
+    await page.click('//button[normalize-space()="Sign up"]')
+    await expect(page).toHaveURL('/login')
+    await page.close()
+  })
+
+})
---
+++ tests/LoginTask.spec.js
@@ -0,0 +1,56 @@
+import { test, expect, describe } from '@playwright/test';
+describe('Login ', async () => {
+  test("Login and Enroll", async ({ page }) => {
+    await page.goto('/login')
+
+    // Assertions
+    const emailInput1 = page.locator("#email1")
+    await expect(emailInput1).toBeVisible()
+    await expect(emailInput1).toBeEnabled()
+    await emailInput1.fill("jes123@gmail.com")
+    expect(await emailInput1.inputValue()).toBe('jes123@gmail.com')
+
+    const passwordInput1 = page.locator("#password1")
+    await expect(passwordInput1).toBeVisible()
+    await expect(passwordInput1).toBeEnabled()
+    await passwordInput1.fill("Automation")
+    expect(await passwordInput1.inputValue()).toBe('Automation')
+    await page.click('button[type="submit"]')
+
+    // Wait for page to load
+    await page.waitForLoadState('networkidle');
+    await expect(page).toHaveURL("https://freelance-learn-automation.vercel.app/");
+
+    // Wait for products to load
+    await page.locator('button:has-text("Add to Cart")').first().waitFor({ state: 'visible' });
+
+    let added = 0;
+
+    while (added < 3) {
+      await page.locator('button:has-text("Add to Cart")').first().click({ force: true });
+      added++;
+    }
+
+    // console.log(`Added ${added} items to the cart.`);
+
+    // Click on cart
+    await page.click('.cartBtn')
+    await expect(page).toHaveURL('/cart')
+
+    // Click on Enroll Button
+    await page.click('button:has-text("Enroll Now")')
+
+    // Fill the form
+    await page.locator('textarea#address').fill("Manchester")
+    await page.locator('input#phone').fill('0123456789')
+    // Click on Enroll Button after filling the form
+    await page.locator('button.action-btn').last().click()
+
+    // capture the text
+    const EnrollId = await page.locator('h4.uniqueId').textContent();
+
+    // cancel button
+    await page.locator('button.action-btn.white-action-btn', { hasText: 'Cancel' }).click()
+
+  })
+})
